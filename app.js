25c08bfa692f9809a2de66e5e1d01ccf84d48805
Praneeth <praneethhosallikar@gmail.com>
2025-08-02 18:12:12 +0530

    first code using jsx

--- app.js
+++ app.js
@@ -1,39 +1,11 @@
-// const heading = document.createElement("h1")
-// const apple = document.createElement("h1")
-// heading.innerHTML= ("Hello World from JS")
-// const place = document.getElementById("root")
-// place.appendChild(heading);
-// apple.innerHTML = ("Praneeth");
-// place.appendChild(apple);
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// jsx
 
-const heading = React.createElement("h1" ,{id : "heading"} , "Hello from React")
-// console.log(heading)
-const place = ReactDOM.createRoot(document.getElementById("root"))
-// console.log(place)
-place.render(heading);
+const h1 = <h1> Namaste React using JSX</h1>;
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-const box = React.createElement(
-    "div" , {id:"Parent"} ,
-   [
-     React.createElement("div",{id:"child"} ,
-        [React.createElement("h1" , {id:"nestedheading"} , 
-            "Hello from Nested React") , React.createElement("h2" , {id:"nestedheading2"} , 
-            "Hello from Nested 2  React")]
-         )
-        ,
-     React.createElement("div",{id:"child2"} ,
-        [React.createElement("h1" , {id:"nestedheading"} , 
-            "Hello from Nested React") , React.createElement("h2" , {id:"nestedheading2"} , 
-            "Hello from Nested 2  React ")]
-         )
-   ] )
-
-console.log(box);
-const place2 = ReactDOM.createRoot(document.getElementById("root2"));
-place2.render(box);
+root.render(h1);
 
