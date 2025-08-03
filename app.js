a034bd529c1f6f4dbe2864c0f4971c91a9c3d447
Praneeth <praneethhosallikar@gmail.com>
2025-08-03 23:36:27 +0530

    react func component

--- app.js
+++ app.js
@@ -3,9 +3,30 @@ import ReactDOM from "react-dom/client";
 
 // jsx
 
-const h1 = <h1> Namaste React using JSX</h1>;
+const h1 = (<h1> Namaste React using JSX</h1>);
+
+
+// react fun comp :
+
+const Headingcompoment2 = () =>(
+    <>
+    <div>  
+        <h1>React funtional component 1 </h1> 
+        <h1>React funtional component  2</h1>
+    </div>
+    <div>  
+        <h1>React funtional component1 </h1> 
+        <h1>React funtional component2 </h1>
+    </div>
+    
+    </>
+   
+  
+);
+
+// const Headingcompoment2 = ()=> <h1> React funtinal component </h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(h1);
+root.render(< Headingcompoment2/>);
 
