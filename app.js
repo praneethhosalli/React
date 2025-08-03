9be0c4711c16b70280e3bcb8126a0abcf8a623c9
Praneeth <praneethhosallikar@gmail.com>
2025-08-03 23:47:10 +0530

    comp composition

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// jsx
-
 const h1 = (<h1> Namaste React using JSX</h1>);
 
 
 // react fun comp :
 
+const Head = () =>(
+    <h1>Hello World</h1>
+
+);
+
 const Headingcompoment2 = () =>(
+
+    // component composition 
     <>
     <div>  
         <h1>React funtional component 1 </h1> 
         <h1>React funtional component  2</h1>
     </div>
+    
+    < Head/>
+
     <div>  
         <h1>React funtional component1 </h1> 
         <h1>React funtional component2 </h1>
