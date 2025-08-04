46466ccf4d1c2b0769472fe096b0465338f461cf
Praneeth <praneethhosallikar@gmail.com>
2025-08-04 18:46:17 +0530

    props destruct ucturing on the fly

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ const res_card = {
     backgroundColor:"#f0f0f0"
 }
 
-const Res_card = (props) =>{
-    console.log(props);
+const Res_card = ({resname , cuisine , stars , est}) =>{
+    // console.log(props);
     return (
         <div className="res_card" style={res_card} >
         <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597" />
-        <h3 > {props.resname}</h3>
-        <h4>{props.cuisine}</h4>
-        <h4>{props.stars}</h4>
-        <h4>{props.est}</h4>
+        <h3 > {resname}</h3>
+        <h4>{cuisine}</h4>
+        <h4>{stars}</h4>
+        <h4>{est}</h4>
     </div>
 
     )
