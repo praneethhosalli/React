8d521b09cc96768518ddd0585d7088e57d2180c5
Praneeth <praneethhosallikar@gmail.com>
2025-08-05 21:36:15 +0530

    filtering in the actual mock data

--- src/components/Body.js
+++ src/components/Body.js
@@ -78,110 +78,7 @@ import { useState } from "react";
 
 
 const Body = ()=>{
-    const [listofres , setlistofres] = useState([
-        {
-                                        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-                                        "info": {
-                                            "id": "726147",
-                                            "name": "Bakingo",
-                                            "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/3/0c6264f7-050e-4ebf-afa4-5c41f9ee9c13_726147.JPG",
-                                            "locality": "Dilsukh Nagar",
-                                            "areaName": "Saroornagar",
-                                            "costForTwo": "₹299 for two",
-                                            "cuisines": [
-                                                "Bakery",
-                                                "Desserts",
-                                                "Beverages",
-                                                "Snacks"
-                                            ],
-                                            "avgRating": 4.6,
-                                            "parentId": "3818",
-                                            "avgRatingString": "4.6",
-                                            "totalRatingsString": "9.1K+",
-                                            "sla": {
-                                                "deliveryTime": 44,
-                                                "lastMileTravel": 7.9,
-                                                "serviceability": "SERVICEABLE",
-                                                "slaString": "40-45 mins",
-                                                "lastMileTravelString": "7.9 km",
-                                                "iconType": "ICON_TYPE_EMPTY"
-                                            },
-                                            "availability": {
-                                                "nextCloseTime": "2025-08-05 00:00:00",
-                                                "opened": true
-                                            }
-
-                                        }
-    },
-    {
-                                        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-                                        "info": {
-                                            "id": "726149",
-                                            "name": "Bakingo",
-                                            "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/3/0c6264f7-050e-4ebf-afa4-5c41f9ee9c13_726147.JPG",
-                                            "locality": "Dilsukh Nagar",
-                                            "areaName": "Saroornagar",
-                                            "costForTwo": "₹299 for two",
-                                            "cuisines": [
-                                                "Bakery",
-                                                "Desserts",
-                                                "Beverages",
-                                                "Snacks"
-                                            ],
-                                            "avgRating": 4.1,
-                                            "parentId": "3818",
-                                            "avgRatingString": "4.6",
-                                            "totalRatingsString": "9.1K+",
-                                            "sla": {
-                                                "deliveryTime": 44,
-                                                "lastMileTravel": 7.9,
-                                                "serviceability": "SERVICEABLE",
-                                                "slaString": "40-45 mins",
-                                                "lastMileTravelString": "7.9 km",
-                                                "iconType": "ICON_TYPE_EMPTY"
-                                            },
-                                            "availability": {
-                                                "nextCloseTime": "2025-08-05 00:00:00",
-                                                "opened": true
-                                            }
-
-                                        }
-    },
-    {
-                                        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-                                        "info": {
-                                            "id": "726148",
-                                            "name": "KFC",
-                                            "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/8/3/0c6264f7-050e-4ebf-afa4-5c41f9ee9c13_726147.JPG",
-                                            "locality": "Dilsukh Nagar",
-                                            "areaName": "Saroornagar",
-                                            "costForTwo": "₹299 for two",
-                                            "cuisines": [
-                                                "Bakery",
-                                                "Desserts",
-                                                "Beverages",
-                                                "Snacks"
-                                            ],
-                                            "avgRating": 3.9,
-                                            "parentId": "3818",
-                                            "avgRatingString": "4.6",
-                                            "totalRatingsString": "9.1K+",
-                                            "sla": {
-                                                "deliveryTime": 44,
-                                                "lastMileTravel": 7.9,
-                                                "serviceability": "SERVICEABLE",
-                                                "slaString": "40-45 mins",
-                                                "lastMileTravelString": "7.9 km",
-                                                "iconType": "ICON_TYPE_EMPTY"
-                                            },
-                                            "availability": {
-                                                "nextCloseTime": "2025-08-05 00:00:00",
-                                                "opened": true
-                                            }
-
-                                        }
-    }
-]);
+    const [listofres , setlistofres] = useState(reslist);
     
     return (
 
@@ -189,7 +86,7 @@ const Body = ()=>{
     <div className="Body">
         <div className="Filter">
             <button className="Filter_button" onClick={()=>{
-                setlistofres()
+                // setlistofres()
                 const filteredres = listofres.filter(
                     (res) => res.info.avgRating>4
                 );
