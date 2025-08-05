802b8af301bc8b07500404d69ea5e5733dbf2ac2
Praneeth <praneethhosallikar@gmail.com>
2025-08-05 20:21:30 +0530

    utils and mock data

---
+++ utils/constance.js
@@ -0,0 +1,4 @@
+export const applogo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-yPxmHYHF8tgtmWhpC59Jk2PLpzbxaL-9bA&s"
+
+export const reslogo = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/"
+
