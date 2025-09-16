85be0c2b37fa26ac17ec04c8a91cd251110375ee
Praneeth <praneethhosallikar@gmail.com>
2025-09-16 23:27:28 +0530

    no promte in data for ep 11

--- src/components/Body.js
+++ src/components/Body.js
@@ -27,7 +27,7 @@ const Body = () => {
     setfilteredres(
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
-    // console.log(json);
+    console.log(json);
   };
 
   const onlineStatus = useOnline();
