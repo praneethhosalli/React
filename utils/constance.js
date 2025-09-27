fca81a021e43d95c468a0896057960b2c9901886
Praneeth <praneethhosallikar@gmail.com>
2025-09-27 21:50:28 +0530

    clean

--- utils/constance.js
+++ utils/constance.js
@@ -4,13 +4,32 @@ export const applogo =
 export const reslogo =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/";
 
-// 20.16.0
-
 export const menuApiUrl =
   "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.3182993&lng=78.5721999&restaurantId=";
 
 export const resfetch =
   "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3182993&lng=78.5721999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 20.16.0
 
 // Instructions to Github copilot
 
