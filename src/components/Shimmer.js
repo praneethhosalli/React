3da4fe54897ed1714bd9902c3fe84d440154fea1
Praneeth <praneethhosallikar@gmail.com>
2025-10-05 00:29:05 +0530

    shimmer tcss

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,21 +1,22 @@
 const Shimmer = () => {
-    return <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+    return <div className="mt-20 flex flex-wrap m-13 ">
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        <div className= " w-50 h-100 bg-gray-200 p-2.5 m-1" ></div>
+        
         
 
     </div>
